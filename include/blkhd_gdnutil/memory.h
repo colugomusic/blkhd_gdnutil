7b9498323bfcf3fceb47a3b2e3d57f67531884df
Colugo <colugomusic@gmail.com>
2020-12-28 00:11:55 +0000

    Create memory.h

---
+++ include/blkhd_gdnutil/memory.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <memory>
+
+namespace blkhd_gdnutil {
+namespace memory {
+
+// Less horrible alternative to using godot::Reference and godot::Ref<T>
+// T should be derived from godot::Object
+// 
+template <class T>
+std::shared_ptr<T> make_new()
+{
+	return std::shared_ptr<T>(T::_new(), [](T* o) { o->free(); });
+}
+
+}}
